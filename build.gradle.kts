219b8c2bc3f1e48fad51d96cf1b2de689d9d465e
Dei <dei@deitech.net>
2024-10-08 14:46:27 -0500

    bluetooth service and test activity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
